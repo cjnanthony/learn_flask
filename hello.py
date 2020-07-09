4092cf946b6e1d7948765502289a7e9143dd4fa9
cjnanthony <cjnanthony@gmail.com>
2020-07-08 23:18:24 -0400

    page 14

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
-from flask import Flask  # flask = package, #Flask = class
+from flask import Flask, render_template  # flask = package, #Flask = class, # render_template used for html
 
 app = Flask(__name__)  # instantiating Flask class--> when running this file, __name__ will be = to __main__
 
 
 @app.route('/')  # function is mapped to the '/' URL which is the home URL
 def home():      # if the route was '/about/' the following home() function would display on the about page not home
-    return "Hey there!"
+    return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':  # if the script is the one being executed
